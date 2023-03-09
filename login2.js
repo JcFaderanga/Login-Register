2ba0b152cbb63495cc8bdc9ade7c801e85c16371
Jeysi/ <119491303+JcFaderanga@users.noreply.github.com>
2023-03-09 21:49:01 +0800

    Add files via upload

--- login2.js
+++ login2.js
@@ -3,26 +3,22 @@ const form = document.querySelectorAll('#register-now');
 form.forEach (swtich => {
  swtich.addEventListener('click', (event) => {	
     if(clicked == true){
-      document.getElementById('title').classList.remove('deactive');
-      document.getElementById('title').classList.add('active');
        setTimeout(function (){
          document.getElementById('login').style.display = "none";
          document.getElementById('register').style.display = "block";
-       },1200);
+       },900);
        document.querySelector('.form-container').classList.remove('remove');
        document.querySelector('.form-container').classList.add('active-width');
        setTimeout(function (){
         document.getElementById('title').innerHTML = "REGISTER";
-       },1000);
+       },900);
       return clicked = false;
     }else{ 
-      document.getElementById('title').classList.remove('active');
-      document.getElementById('title').classList.add('deactive');
      	  setTimeout(function(){
           document.getElementById('login').style.display = "block";
 	        document.getElementById('register').style.display = "none";
           document.getElementById('title').innerHTML = "LOGIN";
-       	},1000);
+       	},900);
        document.querySelector('.form-container').classList.remove('active-width');
        document.querySelector('.form-container').classList.add('remove');
       return clicked = true;
